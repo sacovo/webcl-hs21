8eccce11173a3f9f8c4070ede7e71b10b0ccc18e
Sandro Covo <sandro@sandrocovo.ch>
2021-10-18 12:13:25 +0200

    my work

--- week4/person/personStarter.js
+++ week4/person/personStarter.js
@@ -1,14 +1,22 @@
+import {
+  MasterController,
+  SelectionController,
+  MasterView,
+  DetailView,
+} from "./person.js";
 
-import { MasterController, SelectionController, MasterView, DetailView } from './person.js';
-
-const masterController    = MasterController();
+const masterController = MasterController();
 const selectionController = SelectionController();
 
 // create the sub-views, incl. binding
 
-MasterView(masterController, selectionController, document.getElementById('masterContainer'));
-DetailView(selectionController, document.getElementById('detailContainer'));
+MasterView(
+  masterController,
+  selectionController,
+  document.getElementById("masterContainer")
+);
+DetailView(selectionController, document.getElementById("detailContainer"));
 
 // binding of the main view
 
-document.getElementById('plus').onclick    = _ => masterController.addPerson();
+document.getElementById("plus").onclick = (_) => masterController.addPerson();
